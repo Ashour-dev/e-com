b8b659b9954f06e56cae5466df9c747e778eb70a
Ashour100 <interinstitute5@gmail.com>
2022-08-13 17:02:12 +0200

    minor updates

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -3,9 +3,11 @@
 
 @section('content')
 
-    <img class="w-50" src="{{$product['photo']}}" alt="{{$product['photo']}}">
-    <h1>{{$product['brand']}} {{$product['name']}}</h1>
-    <h3>Size Available: {{$product['size']}}</h3>
-    <h3>price: {{$product['price']}}</h3>
+    <div class="w-50 mx-auto text-center">
+        <img class="w-100 mb-4" src="{{$product['photo']}}" alt="{{$product['photo']}}">
+        <h1>{{$product['brand']}} {{$product['name']}}</h1>
+        <h3>Size Available: {{$product['size']}}</h3>
+        <h3>price: {{$product['price']}}</h3>
+    </div>
 
 @endsection
