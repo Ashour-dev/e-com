888730416d926cb850a9fe5fcd9ce99a75e28c96
Ashour100 <interinstitute5@gmail.com>
2022-08-19 17:10:49 +0200

    Vue configuration

---
+++ resources/js/front.js
@@ -0,0 +1,35 @@
+window.axios=require('axios');
+// window.axios.defaults.headers.common['X-Reuested-With']='XMLHttpRequest';
+window.Vue = require('vue');
+
+
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./views/App";
+
+Vue.use(VueRouter);
+
+import Home from './pages/Home';
+import Show from './pages/Show';
+
+const router = new VueRouter({
+    mode:'history',
+    routes:[
+        {
+            path:'/',
+            name:'Home',
+            component:Home
+        },
+        {
+            path:'/apartment/:id',
+            name:'Show',
+            component:Show
+        },
+]
+})
+
+const app = new Vue({
+    el: '#root',
+    render: h=>h(App),
+    router,
+});
