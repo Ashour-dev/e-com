8a438c063f151fbcd56d4e93156cfc61152a2832
Ashour100 <interinstitute5@gmail.com>
2022-08-21 18:28:11 +0200

    Api call made from the show

--- public/js/front.js
+++ public/js/front.js
@@ -1982,7 +1982,31 @@ __webpack_require__.r(__webpack_exports__);
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony default export */ __webpack_exports__["default"] = ({});
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_0__);
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+  name: 'Show',
+  data: function data() {
+    return {
+      id: this.$route.params.id,
+      product: []
+    };
+  },
+  methods: {
+    getProduct: function getProduct() {
+      var _this = this;
+
+      axios__WEBPACK_IMPORTED_MODULE_0___default.a.get("/api/products/".concat(this.id)).then(function (response) {
+        _this.product = response.data;
+        console.log(_this.product);
+      });
+    }
+  },
+  created: function created() {
+    this.getProduct();
+  }
+});
 
 /***/ }),
 
--- resources/js/pages/Show.vue
+++ resources/js/pages/Show.vue
@@ -5,8 +5,27 @@
 </template>
 
 <script>
+import Axios from 'axios'
 export default {
-
+    name:'Show',
+    data(){
+        return{
+            id:this.$route.params.id,
+            product:[],
+        }
+    },
+    methods:{
+        getProduct(){
+            Axios.get(`/api/products/${this.id}`)
+                .then(response=>{
+                    this.product=response.data;
+                    console.log(this.product)
+                })
+        }
+    },
+    created(){
+        this.getProduct();
+    }
 }
 </script>
 
